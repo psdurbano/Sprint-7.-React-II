1ea4af9409a635b739850623c84900adcc086a8f
Pablo <pablosdurbano@gmail.com>
2023-02-16 12:37:09 +0100

    Exercice 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,124 @@
-import React, { useState } from 'react';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   // Definimos el estado inicial de los valores de los checkboxes y el valor total
-  const [checkboxValues, setCheckboxValues] = useState({
-    option1: false,
-    option2: false,
-    option3: false,
+  const [servicios, setServicios] = useState({
+    web: false,
+    seo: false,
+    social: false,
+    paginas: 1,
+    idiomas: 1,
   });
-  const [totalValue, setTotalValue] = useState(0);
 
   // Función que se ejecuta cada vez que se produce un cambio en un checkbox
   const handleCheckboxChange = (event) => {
+    
     // Obtenemos el nombre y el valor del checkbox que ha cambiado
     const { name, checked } = event.target;
 
     // Actualizamos el estado de checkboxValues con el nuevo valor del checkbox que ha cambiado
-    setCheckboxValues({ ...checkboxValues, [name]: checked });
-
-    // Calculamos el nuevo valor total
-    let newValue = totalValue;
-    if (checked) {
-      // Si se ha marcado el checkbox, sumamos su valor al valor total
-      switch (name) {
-        case 'option1':
-          newValue += 500;
-          break;
-        case 'option2':
-          newValue += 300;
-          break;
-        case 'option3':
-          newValue += 200;
-          break;
-        default:
-          break;
-      }
-    } else {
-      // Si se ha desmarcado el checkbox, restamos su valor al valor total
-      switch (name) {
-        case 'option1':
-          newValue -= 500;
-          break;
-        case 'option2':
-          newValue -= 300;
-          break;
-        case 'option3':
-          newValue -= 200;
-          break;
-        default:
-          break;
-      }
-    }
-
-    // Actualizamos el valor total
-    setTotalValue(newValue);
+    setServicios((prevServicios) => ({
+      ...prevServicios,
+      [name]: checked,
+    }));
   };
 
-  // Renderizamos los checkboxes y el total del valor de los servicios
+  // Manejador de cambios para los inputs de número
+  const handleInputNumberChange = (event) => {
+    const { name, value } = event.target;
+
+    // Actualizar el estado de servicios y parsear el valor a un número entero
+    setServicios((prevServicios) => ({
+      ...prevServicios,
+      [name]: parseInt(value),
+    }));
+  };
+
+  // Inicializar la variable total
+  let total = 0;
+
+  // Calcular el precio total en base a los servicios seleccionados
+  if (servicios.web) {
+    total += 500;
+    total += servicios.paginas * servicios.idiomas * 30;
+  }
+
+  if (servicios.seo) {
+    total += 300;
+  }
+
+  if (servicios.social) {
+    total += 200;
+  }
+
+  // Renderizamos el componente
   return (
-    <div>
-      <label>
-        <input
-          type="checkbox"
-          name="option1"
-          checked={checkboxValues.option1}
-          onChange={handleCheckboxChange}
-        />
-        Una página web ($500)
-      </label>
-      <br />
-      <label>
-        <input
-          type="checkbox"
-          name="option2"
-          checked={checkboxValues.option2}
-          onChange={handleCheckboxChange}
-        />
-        Una consultoria SEO ($300)
-      </label>
-      <br />
-      <label>
-        <input
-          type="checkbox"
-          name="option3"
-          checked={checkboxValues.option3}
-          onChange={handleCheckboxChange}
-        />
-        Una campaña de Google ADS ($200)
-      </label>
-      <br />
-      <br />
-      <p>Precio: ${totalValue}</p>
+    <div className="container">
+      <h3>¿Qué quieres hacer?</h3>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            name="web"
+            checked={servicios.web}
+            onChange={handleCheckboxChange}
+          />
+          Una página web ($500)
+        </label>
+        {/* Mostrar opciones adicionales si se selecciona "Una página web" */}
+        {servicios.web && (
+          <div className="web-options">
+            <div className="input-container">
+              <label>
+                Número de páginas:
+                <input
+                  type="number"
+                  name="paginas"
+                  value={servicios.paginas}
+                  onChange={handleInputNumberChange}
+                  min="1"
+                />
+              </label>
+            </div>
+            <div className="input-container">
+              <label>
+                Número de idiomas:
+                <input
+                  type="number"
+                  name="idiomas"
+                  value={servicios.idiomas}
+                  onChange={handleInputNumberChange}
+                  min="1"
+                />
+              </label>
+            </div>
+          </div>
+        )}
+      </div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            name="seo"
+            checked={servicios.seo}
+            onChange={handleCheckboxChange}
+          />
+          Una consultoría SEO ($300)
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            name="social"
+            checked={servicios.social}
+            onChange={handleCheckboxChange}
+          />
+          Una campaña de Google ADS ($200)
+        </label>
+      </div>
+      <p>Precio: ${total}</p>
     </div>
   );
 }
