f92c3720d05f8adaa889357fe5379cfcfbe56cf8
Pablo <pablosdurbano@gmail.com>
2023-03-14 11:49:23 +0100

    Exercise 5

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import Home from "./pages/Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />;
+      <Route path="/App" element={<App />} />;
+    </Routes>
+  </BrowserRouter>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link, Route } from "react-router-dom";
+import "../App.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h3 className="h3">
+        ¡Bienvenido/a a nuestra aplicación de cálculo de presupuesto de una
+        página web!
+      </h3>
+      <p className="p">
+        Nuestra aplicación te ayuda a calcular el presupuesto para crear la
+        página web que deseas. Personaliza tu proyecto seleccionando
+        características como tipo de sitio, número de páginas y diseño, y el
+        precio total se actualizará automáticamente. También puedes agregar
+        servicios adicionales como alojamiento web y optimización para motores
+        de búsqueda. Al final, verás el presupuesto total y podrás decidir si
+        continuar con la creación de tu sitio web.
+      </p>
+      <button className="button">
+      <Link className="button-Link" to="/App">Iniciar</Link>
+      </button>
+    </div>
+  );
+}
